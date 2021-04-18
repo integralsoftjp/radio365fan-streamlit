e541eaa8d4d08e5cd58bdbb853f20c2259c36530
matzoka <matzoka@outlook.jp>
2021-04-19 07:26:17 +0900

    mongodb add

--- main.py
+++ main.py
@@ -2,15 +2,20 @@
 
 import datetime
 import io
+import pickle
 from PIL import Image
+from functools import wraps
 
 import streamlit as st
 import streamlit.components.v1 as componentsv1
 import feedparser
 import pandas as pd
 import numpy as np
+import pymongo
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from pydub import AudioSegment
+from pymongo.errors import DuplicateKeyError
 
 
 domain = "https://www.radio365.net"
@@ -45,38 +50,75 @@ program_titles = []
 program_subtitles = []
 program_djnames = []
 program_pubdates = []
-program_times = []
-program_images = []
+program_sound_times = []
+program_image_urls = []
 program_summarys = []
 program_sound_urls = []
 
 dj_img_datas = []
+CONN_URI = "mongodb://localhost:27017"
+
+# @st.cache(hash_funcs={MongoClient: id})
+# def get_client():
+#     return  MongoClient()
+
+def provide_db_connection(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        conn = pymongo.MongoClient(CONN_URI)
+        result =  func(conn=conn, *args, **kwargs)
+        conn.close()
+        return result
+    return wrapper
 
 @st.cache(suppress_st_warning=True)
-def read_image():
-    image_url = add_listnew[selector][0]
-    img_data = io.BytesIO(urlopen(image_url).read())
+def read_image(program_image_url:str) -> bytes:
+    img_data = io.BytesIO(urlopen(program_image_url).read())
     return img_data
 
-@st.cache(suppress_st_warning=True)
-def read_sound_data():
-    sound_url = add_listnew[selector][1]
-    sound_data = io.BytesIO(urlopen(sound_url).read())
+# @st.cache(suppress_st_warning=True)
+def read_sound_data(program_sound_url:str) -> bytes:
+    sound_data = io.BytesIO(urlopen(program_sound_url).read())
     return sound_data
 
-def read_sidebar_photos():
+def get_sound_time(sound_url:str, sound_data:bytes) -> str:
+    au_sound = AudioSegment.from_file(sound_data, sound_url[-3:])
+    au_time = au_sound.duration_seconds
+    au_time_str = str(int(au_time / 60)) + ":" + str(int(au_time % 60))
+    return au_time_str
+
+@st.cache
+@provide_db_connection
+def read_sidebar_photos(conn) -> bytes:
     global dj_img_datas
+    db = conn.test
+    image_df = pd.DataFrame.from_records(db.image_df.find())
     for i, dj_image_url in enumerate(dj_images_urls):
-        dj_img_datas.append(io.BytesIO(urlopen(dj_image_url).read()))
+        try:
+            mongo_key = "image_" + dj_ids[i]
+            cnt = db.image_df.count_documents({'name': mongo_key})
+            if cnt > 0:
+                filter_one_imagedata = db.image_df.find({'name': mongo_key})
+                pickled = filter_one_imagedata[0].get('imagedata')
+                image_data = pickle.loads(pickled)
+                dj_img_datas.append(image_data)
+            else:
+                image_data = io.BytesIO(urlopen(dj_image_url).read())
+                dj_img_datas.append(image_data)
+                # pickle
+                pickled = pickle.dumps(image_data)
+                db.image_df.insert( { 'name':mongo_key, 'imagedata': pickled } )
+        except DuplicateKeyError:
+            print('Name duplication error!')
     return dj_img_datas
 
-def set_hrefs():
+def set_hrefs(dj_hrefs:list) -> list:
     htmls = []
     for i, href in enumerate(dj_hrefs):
         if href in '':
-            htmls.append(f'<img src="{dj_images_urls[i]}" alt="dj" width="{html_width}" style="border:solid 2px #FFFFFF">')
+            htmls.append(f'<img src="{dj_images_urls[i]}" alt="dj" width="{html_width}" style="border:solid 1px #FFFFFF">')
         else:
-            htmls.append(f'<a href="{dj_hrefs[i]}" target="_blank"><img src="{dj_images_urls[i]}" alt="dj" width="{html_width}" style="border:solid 2px #FFFFFF"></a>')
+            htmls.append(f'<a href="{dj_hrefs[i]}" target="_blank"><img src="{dj_images_urls[i]}" alt="dj" width="{html_width}" style="border:solid 1px #FFFFFF"></a>')
     return htmls
 
 dj_contents = dj_soup.select("div .prof dl")
@@ -106,38 +148,38 @@ for element in dj_contents:
     el1 = element.get_text().split('\n')[1:-1]
     name = el1[0].split('：')[1]
     dj_names.append(name)
-    # -- 担当番組
+    # -- 担当番組Program
     contents = dj_dicts[name]
     if '担当番組' in contents:
         dj_haveprograms.append(contents['担当番組'])
     else:
         dj_haveprograms.append('')
 
-    # -- 誕生日
+    # -- 誕生日Birth
     if '誕生日' in contents:
         dj_births.append(contents['誕生日'])
     else:
         dj_births.append('')
 
-    # -- 血液型
+    # -- 血液型Blood
     if '血液型' in contents:
         dj_bloods.append(contents['血液型'])
     else:
         dj_bloods.append('')
 
-    # -- 身長
+    # -- 身長Hight
     if '身長' in contents:
         dj_heights.append(contents['身長'])
     else:
         dj_heights.append('')
 
-    # -- 趣味
+    # -- 趣味Hobbys
     if '趣味' in contents:
         dj_hobbys.append(contents['趣味'])
     else:
         dj_hobbys.append('')
 
-    # -- 特技
+    # -- 特技Skills
     if '特技' in contents:
         dj_skills.append(contents['特技'])
     else:
@@ -158,7 +200,6 @@ df = pd.DataFrame({
 df.index = np.arange(1, len(df)+1)
 df = df.style.set_properties(**{'text-align': 'left'})
 
-
 #================================================================
 #================================================================
 #================================================================
@@ -170,13 +211,13 @@ for cnt, entrie in enumerate(program_res.entries):
     program_ids.append(program_id)
     program_titles.append(str(cnt+1) + ": " + entrie['title'])
     program_subtitles.append(entrie['mobilesubtitle'])
-    program_images.append(entrie['mobileimg']['src'])
+    program_image_urls.append(entrie['mobileimg']['src'])
     program_summarys.append(entrie['summary'])
     program_sound_urls.append(program_base + mark + href)
     datestr = entrie['published']
     program_pubdates.append(datetime.datetime.strptime(datestr, '%a, %d %b %Y %H:%M:%S %z').strftime('%Y/%m/%d'))
     program_djnames.append('　')
-    program_times.append('　')
+    program_sound_times.append('　')
 
 # program_djnames 追加
 for i, dj_id in enumerate(program_ids):
@@ -184,20 +225,17 @@ for i, dj_id in enumerate(program_ids):
         program_djnames[i] = dj_names[dj_ids.index(dj_id)]
     else:
         program_djnames[i] = ''
-        
+
 df2 = pd.DataFrame({
 'id': program_ids,
 'Program': program_titles,
 '番組名': program_subtitles,
 'DJName': program_djnames,
 '配信日Pubday': program_pubdates,
-'再生時間SoundTime': program_times
+'再生時間SoundTime': program_sound_times
 })
 df2.index = np.arange(1, len(df2)+1)
 df2 = df2.style.set_properties(**{'text-align': 'left'})
-
-ziplist = list(zip(program_images , program_sound_urls, program_summarys, program_subtitles, program_djnames))
-add_listnew = dict(zip(program_titles, ziplist))
 
 st.title("RADIO365 DJ's Fan site")
 
@@ -209,47 +247,50 @@ Radio365 DJ ファンサイトです。
 st.text(hedder_text)
 
 selector = st.sidebar.selectbox("Select program (1 - 100):",program_titles)
+select_indexNumber = int(selector.split(':')[0])-1
 
-markdown_str = "#### " + '<font color="Gray">' + add_listnew[selector][3] + '</font>'
+markdown_str = "#### " + '<font color="Gray">' + program_subtitles[select_indexNumber] + '</font>'
 st.markdown(markdown_str, unsafe_allow_html=True)
 
 #===== read sidebar image file =======
-img_data = read_image()
+img_data = read_image(program_image_urls[select_indexNumber])
 st.image(img_data, caption=selector, use_column_width=True)
 
 # sound update
-sound_data = read_sound_data()
+sound_data = read_sound_data(program_sound_urls[select_indexNumber])
 st.audio(sound_data, format='audio/aac')
 
-st.markdown(add_listnew[selector][2], unsafe_allow_html=True)
+st.markdown(program_summarys[select_indexNumber], unsafe_allow_html=True)
 
 # DJ photo & profile
 
 html_width = 80
-htmls = set_hrefs()
+htmls = set_hrefs(dj_hrefs)
 html_height = (int(len(htmls) / 8) + 1) * 70
 joinhtml = "".join(htmls)
 html = f"""{joinhtml}"""
 with st.beta_expander("DJ Photo (please click to see profile)",expanded=True):
     componentsv1.html(html,height = html_height)
 
-# DJ List
-if dj_img_datas == []:
-    with st.beta_expander("DJ List",expanded=True):
-        st.dataframe(df)
-
-# Program List
-if dj_img_datas == []:
-    with st.beta_expander('Program List',expanded=True):
-        st.dataframe(df2)
-
 st.sidebar.text('')
 
 # Sidebar DJ photo
-temp_text = str(len(dj_images_urls)) + " dj"
+temp_text = str(len(dj_images_urls)) + " DJ member:"
 st.sidebar.text(temp_text)
 
-if dj_img_datas == []:
-    dj_img_datas = read_sidebar_photos()
-    for i, dj_image_url in enumerate(dj_images_urls):
-        st.sidebar.image(dj_img_datas[i], caption=dj_names[i], use_column_width='True')
+dj_img_datas = read_sidebar_photos()
+for i, dj_image_url in enumerate(dj_images_urls):
+    st.sidebar.image(dj_img_datas[i], caption=dj_names[i], use_column_width='True')
+
+# for i, program_sound_url in enumerate(program_sound_urls):
+#     sound_data = read_sound_data(program_sound_urls[i])
+#     program_sound_times[i] = get_sound_time(program_sound_urls[i], sound_data)
+
+# DJ List
+with st.beta_expander("DJ List",expanded=True):
+    st.dataframe(df)
+
+# Program List
+with st.beta_expander('Program List',expanded=True):
+    st.dataframe(df2)
+    
